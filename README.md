0f8715d5342d66780ce313b2d532f2a1e429f966
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-05-18 11:48:05 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Authentication-with-Appwrite
+ I am create a login, signup and addition update password work flow form complete authentication with appwrite using vite and tailwind css.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl p-4 bg-slate-600 text-white">
+        Authentication with Appwrite
+      </h1>
+    </>
+  );
+}
+
+export default App;
